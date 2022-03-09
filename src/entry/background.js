48e9a56fd67be6e18370c3d856b4c824f4ffed2e
catmade <catmade@qq.com>
2022-03-09 20:23:55 +0800

    feat: introduce storage

--- src/entry/background.js
+++ src/entry/background.js
@@ -1,1 +1,31 @@
-console.log('hello world background todo something~')
+export const config = {
+    get: async function (key) {
+        return (await getLocalStorageValue(key))[key];
+    },
+    set: function (key, value) {
+        let obj = {}
+        obj[key] = value
+        chrome.storage.sync.set(obj);
+    }
+}
+
+async function getLocalStorageValue(key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.sync.get(key, function (value) {
+                resolve(value);
+            })
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+}
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+        console.log(
+            `Storage key "${key}" in namespace "${namespace}" changed.`,
+            `Old value was "${oldValue}", new value is "${newValue}".`
+        );
+    }
+});
--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,22 +1,43 @@
 <template>
-  <div class="main_app">
-      <hr>
-      <div style="display: flex; align-items: center; justify-content: space-between">
-          <div>Header Sticky</div>
-          <el-switch v-model="headerSticky" active-color="#13ce66"/>
-      </div><hr>
-  </div>
+    <div class="main_app">
+        <div style="display: flex; align-items: center; justify-content: space-between">
+            <div>Sticky Header</div>
+            <el-switch v-model="headerSticky" @change="set(headerStickyTag, headerSticky)" active-color="#13ce66"/>
+        </div>
+        <div style="display: flex; align-items: center; justify-content: space-between">
+            <div>Sticky Repository Header</div>
+            <el-switch v-model="repoHeaderSticky" @change="set(repoHeaderStickyTag, repoHeaderSticky)"
+                       active-color="#13ce66"/>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'popup-view',
-    data () {
-    return {
-        headerSticky: true,
+import {config} from "@/entry/background";
+
+let app = {
+    name: 'popup-view',
+    data() {
+        return {
+            headerSticky: false,
+            headerStickyTag: "config.sticky.header",
+            repoHeaderSticky: false,
+            repoHeaderStickyTag: "config.sticky.repo-header",
+        }
+    },
+    methods: {
+        set(k, v) {
+            config.set(k, v);
+        },
+    },
+    async created() {
+        console.log("mounted")
+        this.headerSticky = await config.get(this.headerStickyTag)
+        this.repoHeaderSticky = await config.get(this.repoHeaderStickyTag)
     }
-  }
 }
+
+export default app
 
 </script>
 
